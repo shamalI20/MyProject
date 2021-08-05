dafb7a7121a4f9f3786b2a81b0a35bcc401dd0d6
shamlindurkar12 <shamlindurkar12@ip-172-31-64-97.ec2.internal>
2021-08-05 15:23:19 +0000

    Commit 2

--- Simplilearn_Phase1-Java-Fundamentals-24-7-21/src/com/simplilearn/typecasting/Example2.java
+++ Simplilearn_Phase1-Java-Fundamentals-24-7-21/src/com/simplilearn/typecasting/Example2.java
@@ -3,7 +3,44 @@ package com.simplilearn.typecasting;
 public class Example2 {
 
 	public static void main(String[] args) {
-		// TODO WAP for converting long, float , int => byte , short double.
+		// TODO WAP for converting long, float , int => byte , short, double.
+		long l = 1000;
+		byte b = (byte)l;
+		short s =(short)l;
+		double d = l;
+		System.out.println("Long value is : "+l);
+		System.out.println("Long value to Byte: "+b);
+		System.out.println("Long value to Short is   "+s);
+		System.out.println("Long value to Double is : "+d);
+		
+		System.out.println("-------------------");
+		
+		// Float converted to Byte, Short, Double.
+		float lon = 10;
+		byte by = (byte)lon;
+		short sh =(short)lon;
+		double dou = lon;
+		System.out.println("Float value is : "+lon);
+		System.out.println("Float value converted to Byte: "+by);
+		System.out.println("Float value converted Short is : "+sh);
+		System.out.println("Float value converted to Double is : "+dou);
+		
+		System.out.println("---------------------");
+		
+		// Float converted to Byte, Short, Double.
+				int i = 1000;
+				byte byt = (byte)i;
+				short shor =(short)i;
+				double doub = i;
+				System.out.println("Integer value is : "+i);
+				System.out.println("int value converted to Byte: "+byt); // shows default value
+				System.out.println("int value converted Short is : "+shor);
+				System.out.println("int value converted to Double is : "+doub);		
+		
+		
+		
+		
+		
 
 	}
 
--- Simplilearn_Phase1-Java-Fundamentals-24-7-21/src/com/simplilearn/typecasting/Example3.java
+++ Simplilearn_Phase1-Java-Fundamentals-24-7-21/src/com/simplilearn/typecasting/Example3.java
@@ -1,10 +1,20 @@
 package com.simplilearn.typecasting;
 
+import java.util.Scanner;
+
 public class Example3 {
 
 	public static void main(String[] args) {
 		// TODO :: WAP for string to number value (byte , short , int, long double , float)
         // collect user input from console -> 
+		
+		//int userInput = input.nextInt();
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter a number vaule");
+		int userInput = input.nextInt();
+		
+		
+		
 	}
 
 }
